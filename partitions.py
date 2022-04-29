09b66d26cac98a7a7594b8b860844e2676d440bd
SantiagoIvan <48731203+SantiagoIvan@users.noreply.github.com>
2022-04-29 13:20:21 -0300

    Update partitions.py

--- partitions.py
+++ partitions.py
@@ -9,9 +9,9 @@ def partitions(total):
     
     for row in range(1, len(integers)+1):
         for col in range(1, total+1):
-            aux1 = subproblems_arr[row - 1][col] if (row - 1)>=0 else 0
-            last_integer_inserted = integers[:row][-1] if len(integers[:row])>0 else 0 
-            aux2 = subproblems_arr[row][col-last_integer_inserted] if (col-last_integer_inserted) >= 0 else 0
+            aux1 = subproblems_arr[row - 1][col] if (row - 1)>=0 else 0 # soluciones sin tener en cuenta la 'moneda actual' que den la suma total hasta esa columna
+            last_integer_inserted = integers[row-1] # equivalente a pensar en cual es la moneda que estoy intentando insertar.
+            aux2 = subproblems_arr[row][col-last_integer_inserted] if (col-last_integer_inserted) >= 0 else 0 # soluciones hasta total - ultima moneda
             subproblems_arr[row][col] = aux1 + aux2
             
     return subproblems_arr[-1][-1]
